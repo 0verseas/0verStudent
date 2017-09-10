2132dd3fde3ffee1dd96ccd27a7990c355abfaad
lemon5920 <lemon5920@gmail.com>
2017-09-11 01:20:28 +0800

    申請志願畫面。

---
+++ js/admissionSelection.js
@@ -0,0 +1,25 @@
+var admissionSelection = (function () {
+
+    /**
+	 * cache DOM
+	 */
+    var $grid = $('.grid');
+
+    /**
+	 * init
+	 */
+
+    _init();
+
+    /**
+	 * bind event
+	 */
+
+    function _init() {
+        Sortable.create(sortTrue, {
+            group: "sorting",
+            sort: true
+        });
+    }
+
+})();
