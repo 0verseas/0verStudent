9be18fe6ade2054321a13ab8f9d1a27a66111d0b
lemon5920 <lemon5920@gmail.com>
2017-09-11 10:49:33 +0800

    報名系統志願檢視畫面。

---
+++ js/result.js
@@ -0,0 +1,22 @@
+var result = (function () {
+
+    /**
+	 * cache DOM
+	 */
+    
+
+    /**
+	 * init
+	 */
+
+    _init();
+
+    /**
+	 * bind event
+	 */
+
+    function _init() {
+
+    }
+
+})();
