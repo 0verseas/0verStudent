48a5191b03426655534ec822299ce172054cab2e
lemon5920 <lemon5920@gmail.com>
2017-09-15 20:23:56 +0800

    個人基本資料畫面增加「監護人資料」。 另外當父母不詳時，父母填寫表單將隱藏。

--- js/personalInfo.js
+++ js/personalInfo.js
@@ -1,15 +1,69 @@
 var personalInfo = (function () {
 
 	/**
-	 * cache DOM
-	 */
+	*	private variable
+	*/
+
+	let _currentFatherStatus = 1;
+	let _currentMotherStatus = 1;
 
 	/**
-	 * init
-	 */
+	*	cache DOM
+	*/
+
+	const $personalInfoForm = $('#form-personalInfo');
+	// 家長資料
+	const $fatherStatus = $personalInfoForm.find('.fatherStatus');
+	const $motherStatus = $personalInfoForm.find('.motherStatus');
+	const $fatherDataForm = $personalInfoForm.find('#form-fatherData');
+	const $motherDataForm = $personalInfoForm.find('#form-motherData');
+	const $guardianForm = $personalInfoForm.find('#form-guardian');
+	
 
 	/**
-	 * bind event
-	 */
+	*	init
+	*/
+
+	_init();
+
+	/**
+	*	bind event
+	*/
+
+	$fatherStatus.on('change', _switchFatherDataForm);
+	$motherStatus.on('change', _switchMotherDataForm);
+	
+
+	function _init() {
+		_switchGuardianForm();
+	}
+
+	function _switchFatherDataForm() {
+		_currentFatherStatus = Number($(this).val());
+		if (_currentFatherStatus === 3) {
+			$fatherDataForm.fadeOut();
+		} else {
+			$fatherDataForm.fadeIn();
+		}
+		_switchGuardianForm();
+	}
+
+	function _switchMotherDataForm() {
+		_currentMotherStatus = Number($(this).val());
+		if (_currentMotherStatus === 3) {
+			$motherDataForm.fadeOut();
+		} else {
+			$motherDataForm.fadeIn();
+		}
+		_switchGuardianForm();
+	}
+
+	function _switchGuardianForm() {
+		if (_currentFatherStatus === 3 && _currentMotherStatus === 3) {
+			$guardianForm.fadeIn();
+		} else {
+			$guardianForm.fadeOut();
+		}
+	}
 
 })();
