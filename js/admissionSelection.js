ee1dc1c9643f17dc3f0c57049380e4d299a4d829
lemon5920 <lemon5920@gmail.com>
2017-09-15 23:33:13 +0800

    志願加上序號直接修改順序的功能。

--- js/admissionSelection.js
+++ js/admissionSelection.js
@@ -1,12 +1,28 @@
 var admissionSelection = (function () {
 
+	let _wishList = [
+	{school: "國立暨南國際大學", dept: "自動化工程系"},
+	{school: "國立暨南國際大學", dept: "工業管理系"},
+	{school: "國立暨南國際大學", dept: "電機與資訊技術系"},
+	{school: "國立暨南國際大學", dept: "多媒體動畫應用系"},
+	{school: "國立暨南國際大學", dept: "行銷與流通管理系"},
+	{school: "國立暨南國際大學", dept: "數位旅遊管理系"},
+	{school: "國立暨南國際大學", dept: "文化創意與設計系"},
+	{school: "國立暨南國際大學", dept: "餐飲管理系"},
+	{school: "國立暨南國際大學", dept: "應用外語系"},
+	{school: "國立暨南國際大學", dept: "福祉科技與服務管理系"},
+	{school: "國立暨南國際大學", dept: "數位生活創意系"}
+	];
+
+	let _prevWishIndex = -1;
+	let _currentWishIndex = -1;
+
 	/**
 	*	cache DOM
 	*/
 
-	var $upArrow = $('.up-arrow');
-	var $downArrow = $('.down-arrow');
-	var $removeWish = $('.remove-wish');
+	var $wishList = $('#wish-list');
+	var $wishRow = $wishList.find('tr');
 
 	/**
 	*	init
@@ -18,25 +34,101 @@ var admissionSelection = (function () {
 	*	bind event
 	*/
 
-	$upArrow.on("click", prevWish);
-	$downArrow.on("click", nextWish);
-	$removeWish.on("click", removeWish);
-
 	function _init() {
-
+		_generateWishList();
 	}
 
-	function prevWish() {
-		$(this).closest('tr').insertBefore($(this).closest('tr').prev());
+	function _findRowIndex(row) {
+		var tableRow = row.closest('tr');
+		var tbody = $("tr.row");
+		var index = tbody.index(tableRow);
+		return index;
 	}
 
-	function nextWish() {
-		$(this).closest('tr').insertAfter($(this).closest('tr').next());
+	function _prevWish() {
+		var rowIndex = _findRowIndex($(this));
+		if (rowIndex > 0) {
+			var swap = _wishList[rowIndex];
+			_wishList[rowIndex] = _wishList[rowIndex-1];
+			_wishList[rowIndex-1] = swap;
+			_generateWishList();
+		}
 	}
 
-	function removeWish() {
-		$(this).closest('tr').remove();
-		// 接上 API 後，需要將移除的志願，還原到招生校系清單的陣列中。
+	function _nextWish() {
+		var rowIndex = _findRowIndex($(this));
+		if (rowIndex < _wishList.length-1) {
+			var swap = _wishList[rowIndex];
+			_wishList[rowIndex] = _wishList[rowIndex+1];
+			_wishList[rowIndex+1] = swap;
+			_generateWishList();
+		}
 	}
-	
+
+	function _removeWish() {
+		var rowIndex = _findRowIndex($(this));
+		_wishList.splice(rowIndex, 1);
+		_generateWishList();
+	}
+
+	function _savePrevWishIndex() {
+		_prevWishIndex = $(this).val() - 1;
+	}
+
+	function _chWishIndex() {
+		var currentNum = $(this).val();
+
+		if (currentNum > _wishList.length) {
+			currentNum = _wishList.length;
+		} else if (currentNum < 1 ) {
+			currentNum = 1;
+		}
+		_currentWishIndex = currentNum - 1;
+
+		var element = _wishList[_prevWishIndex];
+		_wishList.splice(_prevWishIndex, 1);
+		_wishList.splice(_currentWishIndex, 0, element);
+		_generateWishList();
+	}
+
+	function _generateWishList() {
+		var rowHtml = '';
+		for(i in _wishList) {
+			var rowHtml = rowHtml + `
+			<tr class="row">
+			<td class="col-1">
+			<button type="button" class="btn btn-danger btn-sm remove-wish"><i class="fa fa-times" aria-hidden="true"></i></button>
+			</td>
+			<td class="col-7 col-sm-8">
+			` + _wishList[i].school + '<br>' + _wishList[i].dept + `
+			</td>
+			<td class="col-4 col-sm-3 text-right">
+			<div class="input-group">
+			<input type="number" class="form-control wish-num" value="` + (Number(i) + 1) + `">
+			<div class="input-group-btn">
+			<button type="button" class="btn btn-secondary btn-sm up-arrow">
+			<i class="fa fa-chevron-up" aria-hidden="true"></i>
+			</button>
+			<button type="button" class="btn btn-secondary btn-sm down-arrow">
+			<i class="fa fa-chevron-down" aria-hidden="true"></i>
+			</button>
+			</div>
+			</div>
+			</td>
+			</tr>
+			`;
+		}
+		$wishList.html(rowHtml);
+
+		var $removeWish = $('.remove-wish');
+		var $wishNum = $wishList.find('.wish-num');
+		var $upArrow = $('.up-arrow');
+		var $downArrow = $('.down-arrow');
+		$removeWish.on("click", _removeWish);
+		$wishNum.on("focusin", _savePrevWishIndex);
+		$wishNum.on("change", _chWishIndex);
+		$upArrow.on("click", _prevWish);
+		$downArrow.on("click", _nextWish);
+	}
+
 })();
--- js/placementSelection.js
+++ js/placementSelection.js
@@ -1,15 +1,28 @@
 var placementSelection = (function () {
 
+	let _wishList = [
+	{school: "國立暨南國際大學", dept: "自動化工程系"},
+	{school: "國立暨南國際大學", dept: "工業管理系"},
+	{school: "國立暨南國際大學", dept: "電機與資訊技術系"},
+	{school: "國立暨南國際大學", dept: "多媒體動畫應用系"},
+	{school: "國立暨南國際大學", dept: "行銷與流通管理系"},
+	{school: "國立暨南國際大學", dept: "數位旅遊管理系"},
+	{school: "國立暨南國際大學", dept: "文化創意與設計系"},
+	{school: "國立暨南國際大學", dept: "餐飲管理系"},
+	{school: "國立暨南國際大學", dept: "應用外語系"},
+	{school: "國立暨南國際大學", dept: "福祉科技與服務管理系"},
+	{school: "國立暨南國際大學", dept: "數位生活創意系"}
+	];
+
+	let _prevWishIndex = -1;
+	let _currentWishIndex = -1;
+
 	/**
 	*	cache DOM
 	*/
 
-	var $upArrow = $('.up-arrow');
-	var $downArrow = $('.down-arrow');
-	var $removeWish = $('.remove-wish');
 	var $wishList = $('#wish-list');
 	var $wishRow = $wishList.find('tr');
-	var $wishNum = $wishList.find('.wish-num');
 
 	/**
 	*	init
@@ -21,89 +34,101 @@ var placementSelection = (function () {
 	*	bind event
 	*/
 
-	$upArrow.on("click", prevWish);
-	$downArrow.on("click", nextWish);
-	$removeWish.on("click", removeWish);
-	$wishNum.on("focusin", savePrevWishNum);
-	$wishNum.on("change", chWishNum);
-
 	function _init() {
-		refreshWishNum();
+		_generateWishList();
 	}
 
-	function prevWish() {
-		$(this).closest('tr').insertBefore($(this).closest('tr').prev());
-	}
-
-	function nextWish() {
-		$(this).closest('tr').insertAfter($(this).closest('tr').next());
-	}
-
-	function removeWish() {
-		var tableRow = $(this).closest('tr');
+	function _findRowIndex(row) {
+		var tableRow = row.closest('tr');
 		var tbody = $("tr.row");
-		var rowIndex = tbody.index(tableRow);
-
-		// console.log(tableRow);
-		// console.log(tbody);
-		console.log(rowIndex);
-
-
-		// $(this).closest('tr').remove();
-		// 接上 API 後，需要將移除的志願，還原到招生校系清單的陣列中。
+		var index = tbody.index(tableRow);
+		return index;
 	}
 
-	function savePrevWishNum() {
-		console.log("Saving value " + $(this).val());
-		$(this).data('val', $(this).val());
+	function _prevWish() {
+		var rowIndex = _findRowIndex($(this));
+		if (rowIndex > 0) {
+			var swap = _wishList[rowIndex];
+			_wishList[rowIndex] = _wishList[rowIndex-1];
+			_wishList[rowIndex-1] = swap;
+			_generateWishList();
+		}
 	}
 
-	function chWishNum() {
-		var prev = $(this).data('val');
-		var current = $(this).val();
-		$(this).data('val', $(this).val());
-
-		if (current >= $wishRow.length) {
-			current = $wishRow.length;
-			$($wishRow[prev-1]).insertAfter($wishRow[current-1]);
-		} else {
-			$($wishRow[prev-1]).insertBefore($wishRow[current-1]);
+	function _nextWish() {
+		var rowIndex = _findRowIndex($(this));
+		if (rowIndex < _wishList.length-1) {
+			var swap = _wishList[rowIndex];
+			_wishList[rowIndex] = _wishList[rowIndex+1];
+			_wishList[rowIndex+1] = swap;
+			_generateWishList();
 		}
-
-		if (current < prev) {
-			var x = $wishRow[prev-1];
-			for (var i = prev-1; i > current-1 ; i--) {
-				$wishRow[i] = $wishRow[i-1]
-			}
-			$wishRow[current-1] = x;
-		} else {
-			var x = $wishRow[prev-1];
-			for (var i = prev-1; i < current-1 ; i++) {
-				$wishRow[i] = $wishRow[i+1]
-			}
-			$wishRow[current-1] = x;
-		}
-
-		
-
-
-
-		// console.log($wishRow[current-1]);
-		
-		refreshWishNum();
-
-		// console.log($wishRow);
-
-		// console.log("Prev value " + prev);
-		// console.log("New value " + current);
-		
 	}
 
-	function refreshWishNum() {
-		for (var i = 0; i < $wishRow.length; i++) {
-			$($wishRow[i]).find('.wish-num').val(i+1);
+	function _removeWish() {
+		var rowIndex = _findRowIndex($(this));
+		_wishList.splice(rowIndex, 1);
+		_generateWishList();
+	}
+
+	function _savePrevWishIndex() {
+		_prevWishIndex = $(this).val() - 1;
+	}
+
+	function _chWishIndex() {
+		var currentNum = $(this).val();
+
+		if (currentNum > _wishList.length) {
+			currentNum = _wishList.length;
+		} else if (currentNum < 1 ) {
+			currentNum = 1;
 		}
-		console.log($wishRow);
+		_currentWishIndex = currentNum - 1;
+
+		var element = _wishList[_prevWishIndex];
+		_wishList.splice(_prevWishIndex, 1);
+		_wishList.splice(_currentWishIndex, 0, element);
+		_generateWishList();
+	}
+
+	function _generateWishList() {
+		var rowHtml = '';
+		for(i in _wishList) {
+			var rowHtml = rowHtml + `
+			<tr class="row">
+			<td class="col-1">
+			<button type="button" class="btn btn-danger btn-sm remove-wish"><i class="fa fa-times" aria-hidden="true"></i></button>
+			</td>
+			<td class="col-7 col-sm-8">
+			` + _wishList[i].school + '<br>' + _wishList[i].dept + `
+			</td>
+			<td class="col-4 col-sm-3 text-right">
+			<div class="input-group">
+			<input type="number" class="form-control wish-num" value="` + (Number(i) + 1) + `">
+			<div class="input-group-btn">
+			<button type="button" class="btn btn-secondary btn-sm up-arrow">
+			<i class="fa fa-chevron-up" aria-hidden="true"></i>
+			</button>
+			<button type="button" class="btn btn-secondary btn-sm down-arrow">
+			<i class="fa fa-chevron-down" aria-hidden="true"></i>
+			</button>
+			</div>
+			</div>
+			</td>
+			</tr>
+			`;
+		}
+		$wishList.html(rowHtml);
+
+		var $removeWish = $('.remove-wish');
+		var $wishNum = $wishList.find('.wish-num');
+		var $upArrow = $('.up-arrow');
+		var $downArrow = $('.down-arrow');
+		$removeWish.on("click", _removeWish);
+		$wishNum.on("focusin", _savePrevWishIndex);
+		$wishNum.on("change", _chWishIndex);
+		$upArrow.on("click", _prevWish);
+		$downArrow.on("click", _nextWish);
 	}
 
 })();
