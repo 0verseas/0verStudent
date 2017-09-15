8b4a281bfcc69bfb80934ff22b4918fe754ae047
YY <z3388638@gmail.com>
2017-09-15 22:18:29 +0800

    add systemChoose page

---
+++ js/systemChoose.js
@@ -0,0 +1,21 @@
+(() => {
+	/**
+	 * cache DOM
+	 */
+	const $SystemChoose = $('.SystemChoose');
+	const $nextBtn = $SystemChoose.find('.SystemChoose__btnNext');
+	const $system = $SystemChoose.find('#SystemChoose__inputSystem');
+
+	/**
+	 * bind event
+	 */
+	$nextBtn.on('click', _handleSubmit);
+
+	/**
+	 * private method
+	 */
+	function _handleSubmit() {
+		const system = $system.val();
+		console.log(system);
+	}
+})();
