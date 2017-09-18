cd723610bf6618923d2bdd7a87c533a85cae04e5
YY <z3388638@gmail.com>
2017-09-18 21:21:18 +0800

    fix index

---
+++ js/index.js
@@ -0,0 +1,24 @@
+(() => {
+	/**
+	 * cache DOM
+	 */
+	const $email = $('#email');
+	const $pass = $('#password');
+	const $loginBtn = $('#btn-login');
+	
+	/**
+	 * bind event
+	 */
+	$loginBtn.on('click', _handleLogin);
+
+	/**
+	 * event handlet
+	 */
+	function _handleLogin() {
+		const email = $email.val();
+		const pass = $pass.val();
+
+		console.log([email, pass]);
+		location.href = './systemChoose.html';
+	}
+})();
