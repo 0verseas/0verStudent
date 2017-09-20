e37247a452e526f04bfb0ca7f42c894eefb985f9
lemon5920 <lemon5920@gmail.com>
2017-09-21 01:18:55 +0800

    志願清單篩選功能。 四種類別篩選、志願數量限制，以及跨選邏輯： 奧林匹亞：所有類組可跨選。 個人申請、聯合分發：二三可跨選，一不可跨選。

--- js/olympia.js
+++ js/olympia.js
@@ -4,19 +4,21 @@
 	*	private variable
 	*/
 
-	let _wishList = [
-	{school: "國立暨南國際大學", dept: "自動化工程系"},
-	{school: "國立暨南國際大學", dept: "工業管理系"},
-	{school: "國立暨南國際大學", dept: "電機與資訊技術系"},
-	{school: "國立暨南國際大學", dept: "多媒體動畫應用系"},
-	{school: "國立暨南國際大學", dept: "行銷與流通管理系"},
-	{school: "國立暨南國際大學", dept: "數位旅遊管理系"},
-	{school: "國立暨南國際大學", dept: "文化創意與設計系"},
-	{school: "國立暨南國際大學", dept: "餐飲管理系"},
-	{school: "國立暨南國際大學", dept: "應用外語系"},
-	{school: "國立暨南國際大學", dept: "福祉科技與服務管理系"},
-	{school: "國立暨南國際大學", dept: "數位生活創意系"}
+	let _optionalWish = [
+	{id:"1001", group: "第一類組", school: "國立暨南國際大學", dept: "中國文學系", engDept: "Dept. of Chinese Literature"},
+	{id:"1002", group: "第一類組", school: "國立暨南國際大學", dept: "外國語文學系", engDept: "Dept. of Foreign Languages and Literatures"},
+	{id:"1003", group: "第一類組", school: "國立暨南國際大學", dept: "歷史學系", engDept: "Dept. of History"},
+	{id:"1004", group: "第二類組", school: "國立暨南國際大學", dept: "哲學系", engDept: "Dept. of Philosophy"},
+	{id:"1005", group: "第二類組", school: "國立暨南國際大學", dept: "人類學系", engDept: "Dept. of Anthropology"},
+	{id:"1006", group: "第二類組", school: "國立暨南國際大學", dept: "圖書資訊學系", engDept: "Dept. of Library and Information Science"},
+	{id:"1007", group: "第二類組", school: "國立暨南國際大學", dept: "日本語文學系", engDept: "Dept. of Japanese Language and Literature"},
+	{id:"1008", group: "第三類組", school: "國立暨南國際大學", dept: "戲劇學系", engDept: "Dept. of Drama and Theatre"},
+	{id:"1008", group: "第三類組", school: "國立暨南國際大學", dept: "法律學系法學組", engDept: "Dept. of Law, Division of Legal Science"},
+	{id:"1009", group: "第三類組", school: "國立暨南國際大學", dept: "政治學系政治理論組", engDept: "Dept. of Political Science, Political Theory Division"},
+	{id:"1010", group: "第三類組", school: "國立暨南國際大學", dept: "經濟學系", engDept: "Dept. of Economics"}
 	];
+
+	let _wishList = [];
 
 	let _prevWishIndex = -1;
 	let _currentWishIndex = -1;
@@ -25,12 +27,13 @@
 	*	cache DOM
 	*/
 
-
 	const $hasOlympiaForm = $('#form-hasOlympia');
 	const $hasOlympia = $hasOlympiaForm.find('.hasOlympia');
 	const $olympiaSelectForm = $('#form-olympiaSelect');
+	const $optionFilterSelect = $('#select-optionFilter');
+	const $optionFilterInput = $('#input-optionFilter'); // 搜尋欄
+	const $optionalWishList = $('#optionalWish-list');
 	const $wishList = $('#wish-list');
-	const $wishRow = $wishList.find('tr');
 
 	/**
 	*	init
@@ -43,9 +46,12 @@
 	*/
 
 	$hasOlympia.on('change', _showWishList);
+	$optionFilterSelect.on('change', _filterOptionalWishList);
+	$optionFilterInput.on('keyup', _filterOptionalWishList); // 列表篩選
 
 	function _init() {
 		student.setHeader();
+		_generateOptionalWish();
 		_generateWishList();
 	}
 
@@ -58,10 +64,38 @@
 		}
 	}
 
+	function _filterOptionalWishList() {
+		const filterSelect = Number($optionFilterSelect.val());
+		const filter = $optionFilterInput.val().toUpperCase();
+		const tr = $optionalWishList.find('tr');
+		for (i = 0; i < tr.length; i++) {
+			let spanVal = $(tr[i].getElementsByTagName("span")[filterSelect]).text();
+			if (spanVal) {
+				if (spanVal.toUpperCase().indexOf(filter) > -1) {
+					tr[i].style.display = "";
+				} else {
+					tr[i].style.display = "none";
+				}
+			}
+		}
+	}
+
+	function _addWish() {
+		if (_wishList.length < 3) {
+			let optionalIndex = $(this).data("optionalindex");
+			_wishList.push(_optionalWish[optionalIndex]);
+			_optionalWish.splice(optionalIndex, 1);
+			_generateOptionalWish();
+			_generateWishList();
+			_filterOptionalWishList();
+		} else {
+			alert('志願數量已達上限。');
+		}
+	}
+
 	function _findRowIndex(row) {
 		const tableRow = row.closest('tr');
-		const tbody = $("tr.row");
-		const index = tbody.index(tableRow);
+		const index = tableRow.data('wishindex');
 		return index;
 	}
 
@@ -87,8 +121,14 @@
 
 	function _removeWish() {
 		const rowIndex = _findRowIndex($(this));
+		_optionalWish.push(_wishList[rowIndex]);
 		_wishList.splice(rowIndex, 1);
+		_optionalWish.sort(function(a, b) {
+		    return parseInt(a.id) - parseInt(b.id);
+		});
+		_generateOptionalWish();
 		_generateWishList();
+		_filterOptionalWishList();
 	}
 
 	function _savePrevWishIndex() {
@@ -111,16 +151,41 @@
 		_generateWishList();
 	}
 
+	function _generateOptionalWish() {
+		let rowHtml = '';
+
+		for(i in _optionalWish) {
+			rowHtml = rowHtml + `
+			<tr>
+			<td>
+			<span>` + _optionalWish[i].id + `</span> ｜ <span>` + _optionalWish[i].group + `</span> ｜ <span>` + _optionalWish[i].school + `</span> <br>
+			<span>` + _optionalWish[i].dept + ` ` + _optionalWish[i].engDept + `</span>
+			</td>
+			<td class="text-right">
+			<button type="button" data-optionalIndex="` + i + `" class="btn btn-info btn-sm add-wish">
+			<i class="fa fa-plus" aria-hidden="true"></i>
+			</button>
+			</td>
+			</tr>
+			`;
+		}
+		$optionalWishList.html(rowHtml);
+
+		const $addWish = $optionalWishList.find('.add-wish');
+		$addWish.on("click", _addWish);
+	}
+
 	function _generateWishList() {
 		let rowHtml = '';
 		for(i in _wishList) {
 			rowHtml = rowHtml + `
-			<tr class="row">
+			<tr class="row" data-wishIndex="` + i + `">
 			<td class="col-1">
 			<button type="button" class="btn btn-danger btn-sm remove-wish"><i class="fa fa-times" aria-hidden="true"></i></button>
 			</td>
 			<td class="col-7 col-sm-8">
-			` + _wishList[i].school + '<br>' + _wishList[i].dept + `
+			` + _wishList[i].id + ` ｜ ` + _wishList[i].group + ` ｜ ` + _wishList[i].school + ` <br>
+			` + _wishList[i].dept + ` ` + _wishList[i].engDept + `
 			</td>
 			<td class="col-4 col-sm-3 text-right">
 			<div class="input-group">
@@ -140,10 +205,10 @@
 		}
 		$wishList.html(rowHtml);
 
-		const $removeWish = $('.remove-wish');
+		const $removeWish = $wishList.find('.remove-wish');
 		const $wishNum = $wishList.find('.wish-num');
-		const $upArrow = $('.up-arrow');
-		const $downArrow = $('.down-arrow');
+		const $upArrow = $wishList.find('.up-arrow');
+		const $downArrow = $wishList.find('.down-arrow');
 		$removeWish.on("click", _removeWish);
 		$wishNum.on("focusin", _savePrevWishIndex);
 		$wishNum.on("change", _chWishIndex);
