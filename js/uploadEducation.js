84440dcd24ad81ee7d070c175cf1648daf9c034c
lemon5920 <lemon5920@gmail.com>
2017-09-25 17:56:51 +0800

    「上傳學歷證件」畫面完成。 之後備審資料上傳畫面也是比照此頁進行。 1. 點選「新增檔案」後，「已上傳」字樣 => 「待上傳」。 2. 新加入的圖片，以黃色外框表示。 3. 點選「確認上傳」或「下一步」才會將圖片真正存到 DB 中。（「下一步」會驗證學歷證明、成績單是否都有上傳。「確認上傳」會重整頁面。）

--- js/uploadEducation.js
+++ js/uploadEducation.js
@@ -3,6 +3,16 @@
 	/**
 	*	cache DOM
 	*/
+
+	const $certificateFrom = $('#form-certificate');
+	const $addCertificateBtn = $certificateFrom.find('#btn-addCertificate');
+	const $certificateTitle = $certificateFrom.find('#title-certificate');
+	const $certificateImgArea = $certificateFrom.find('#certificateImgArea');
+
+	const $transcriptForm = $('#form-transcript');
+	const $addTranscriptBtn = $transcriptForm.find('#btn-addTranscript');
+	const $transcriptTitle = $transcriptForm.find('#title-transcript');
+	const $transcriptImgArea = $transcriptForm.find('#transcriptImgArea');
 	
 	/**
 	*	init
@@ -14,6 +24,9 @@
 	*	bind event
 	*/
 
+	$addCertificateBtn.on("click", _addCertificate);
+	$addTranscriptBtn.on("click", _addTranscript);
+
 	function _init() {
 		student.setHeader();
 		
@@ -22,6 +35,20 @@
 			btnClass: "btn-secondary",
 			text: " 選擇圖片"
 		});
+	}
+
+	function _addCertificate() {
+		$certificateTitle.html('待上傳學歷證明');
+		let width = Math.floor((Math.random() * 500) + 200);
+		let height = Math.floor((Math.random() * 500) + 200);
+		$certificateImgArea.append('<img class="img-thumbnail bg-yellow" src="http://via.placeholder.com/' + width + 'x' + height + '" data-toggle="modal" data-target=".img-modal">');
+	}
+
+	function _addTranscript() {
+		$transcriptTitle.html('待上傳成績單');
+		let width = Math.floor((Math.random() * 500) + 200);
+		let height = Math.floor((Math.random() * 500) + 200);
+		$transcriptImgArea.append('<img class="img-thumbnail bg-yellow" src="http://via.placeholder.com/' + width + 'x' + height + '" data-toggle="modal" data-target=".img-modal">');
 	}
 
 })();
