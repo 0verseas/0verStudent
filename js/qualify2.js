55f4b0c39ee4e57cd7ba9b9e0737c39a8e9949d6
YY <z3388638@gmail.com>
2017-09-27 22:41:55 +0800

    港二技 資格檢視 「是否另持有「香港護照或英國國民（海外）護照」以外之旅行證照，或持有澳門護照以外之旅行證照」邏輯

--- js/qualify2.js
+++ js/qualify2.js
@@ -10,12 +10,14 @@
 	const $signUpForm = $('#form-signUp');
 	const $graduatedRadio = $signUpForm.find('.radio-graduated');
 	const $idCardRadio = $signUpForm.find('.radio-idCard');
+	const $holdpassportRadio = $signUpForm.find('.radio-holdpassport');
 
 	/**
 	*	bind event
 	*/
 	$graduatedRadio.on('change', _checkGraduated);
 	$idCardRadio.on('change', _cehckIdCardValidation);
+	$holdpassportRadio.on('change', _checkHoldpassport);
 
 	/**
 	*	event handler
@@ -36,4 +38,30 @@
 		!!idCard || $signUpForm.find('.idCardAlert.invalid').fadeIn();
 	}
 
+	// 是否另持有「香港護照或英國國民（海外）護照」以外之旅行證照，或持有澳門護照以外之旅行證照
+	function _checkHoldpassport() {
+		const $this = $(this);
+		const holdpassport = +$this.val();
+		!!holdpassport && $signUpForm.find('.isTaiwanHousehold, .holdpassportThanShow').fadeIn() && _setTypeOfKangAo(null);
+		!!holdpassport || $signUpForm.find('.isTaiwanHousehold, .holdpassportThanShow').fadeOut() && _setTypeOfKangAo(1);
+	}
+
+	/**
+	*	private method
+	*/
+	function _setTypeOfKangAo(type) {
+		console.log(`Kang Ao type changed: ${type}`);
+		// $KA_hasBeenTaiwanRadio.last().prop('checked', true).trigger('change');
+		switch (type) {
+			case 1:
+				_typeOfKangAo = 1;
+				break;
+			case 2:
+				_typeOfKangAo = 2;
+				break;
+			default:
+				_typeOfKangAo = null;
+				break;
+		}
+	}
 })();
