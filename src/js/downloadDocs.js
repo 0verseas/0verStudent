70947e4640ecbaaabf60dcb102984c1d93a4341b
hewen-lotus <lotus3698@gmail.com>
2017-11-07 21:09:00 +0800

    研究所在台僑生、在台港澳生 修改繳交或郵寄至海外聯合招生委員會

--- src/js/downloadDocs.js
+++ src/js/downloadDocs.js
@@ -1,5 +1,7 @@
 (() => {
 
+	const $memo = $('#memo');
+    const memo = document.getElementById('memo');
 	/**
 	*	init
 	*/
@@ -18,6 +20,35 @@
 
 			$('#btn-smart').attr('href', env.baseUrl + '/students/print-admission-paper');
 			loading.complete();
+
+            student.getStudentRegistrationProgress().then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw res;
+                }
+            })
+			.then((data) => {
+				//console.log("comeoutttttttttt",data.student_qualification_verify);
+				//console.log("system",data.student_qualification_verify.system_id)
+				//console.log("identity",data.student_qualification_verify.identity)
+
+				let $memo="";
+				if( (data.student_qualification_verify.system_id ===3 || data.student_qualification_verify.system_id===4) &&
+                    data.student_qualification_verify.identity > 3){
+                    memo.innerHTML="請在期限內列印並繳交或郵寄至海外聯合招生委員會。";
+				}
+				else
+					memo.innerHTML="請在期限內列印並繳交至駐外機構。";
+
+
+			})
+			.catch((err) => {
+				err.json && err.json().then((data) => {
+					console.error(data);
+					alert(`ERROR: \n${data.messages[0]}`);
+				})
+			});
 
 		} catch (e) {
 			if (e.status && e.status === 401) {
