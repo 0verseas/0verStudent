fdc6fec792c375cec4e53aa431949c41a1fd749c
lemon5920 <lemon5920@gmail.com>
2017-11-07 21:43:50 +0800

    「下載報名表件」頁面改用填寫進度 API 驗證登入狀況。 拔掉之前用 GET /students/login 的方式。減少呼叫次數。

--- src/js/downloadDocs.js
+++ src/js/downloadDocs.js
@@ -17,52 +17,43 @@
 	*/
 
 	async function _init() {
-		try {
 
-			const response = await student.isLogin();
-			if (!response.ok) { throw response; }
+		student.getStudentRegistrationProgress()
+		.then((res) => {
+			if (res.ok) {
+				return res.json();
+			} else {
+				throw res;
+			}
+		})
+		.then((data) => {
+			//console.log("comeoutttttttttt",data.student_qualification_verify);
+			//console.log("system",data.student_qualification_verify.system_id)
+			//console.log("identity",data.student_qualification_verify.identity)
 
+			if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
+				data.student_qualification_verify.identity > 3) {
+				$memo.html("請在期限內列印並繳交或郵寄至海外聯合招生委員會。");
+			} else {
+				$memo.html("請在期限內列印並繳交至駐外機構。");
+			}
+		})
+		.then(() => {
 			$('#btn-smart').attr('href', env.baseUrl + '/students/print-admission-paper');
 			loading.complete();
-
-			student.getStudentRegistrationProgress()
-			.then((res) => {
-				if (res.ok) {
-					return res.json();
-				} else {
-					throw res;
-				}
-			})
-			.then((data) => {
-				//console.log("comeoutttttttttt",data.student_qualification_verify);
-				//console.log("system",data.student_qualification_verify.system_id)
-				//console.log("identity",data.student_qualification_verify.identity)
-
-				if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
-					data.student_qualification_verify.identity > 3) {
-					$memo.html("請在期限內列印並繳交或郵寄至海外聯合招生委員會。");
-				} else {
-					$memo.html("請在期限內列印並繳交至駐外機構。");
-				}
-			})
-			.catch((err) => {
-				err.json && err.json().then((data) => {
-					console.error(data);
-					alert(`ERROR: \n${data.messages[0]}`);
-				})
-			});
-		} catch (e) {
-			if (e.status && e.status === 401) {
+		})
+		.catch((err) => {
+			if (err.status && err.status === 401) {
 				alert('請登入。');
 				location.href = "./index.html";
 			} else {
-				e.json && e.json().then((data) => {
+				err.json && err.json().then((data) => {
 					console.error(data);
 					alert(`ERROR: \n${data.messages[0]}`);
 				})
 			}
 			loading.complete();
-		}
+		});
 	}
 
 })();
