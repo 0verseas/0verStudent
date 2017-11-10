c1c713582606c7bc345bf93ee0f948468bb4f75a
sunnyworm <sunnyworm@gmail.com>
2017-11-10 14:21:59 +0800

    下載頁面增加澳門考古題與報名地點
    外連改為開新分頁

--- src/js/downloadDocs.js
+++ src/js/downloadDocs.js
@@ -30,7 +30,11 @@
 			//console.log("comeoutttttttttt",data.student_qualification_verify);
 			//console.log("system",data.student_qualification_verify.system_id)
 			//console.log("identity",data.student_qualification_verify.identity)
+			if ((data.student_qualification_verify.system_id === 1 || data.student_qualification_verify.system_id === 2)
+				&& (data.student_misc_data.admission_placement_apply_way === 5 || data.student_misc_data.admission_placement_apply_way === 15)) { // 參加澳門學科測驗
 
+				$('.alert-downloadMoFile').show();
+			}
 			if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
 				data.student_qualification_verify.identity > 3) {
 				$memo.html("請在期限內列印並繳交或郵寄至海外聯合招生委員會。");
