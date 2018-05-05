f16fabdb7ac6766ee25503a920c49a4f8dd4ffd9
He-Wen Chen <lotus3698@gmail.com>
2018-05-05 22:39:40 +0800

    除不參加聯合分發，其餘皆可預覽顯示聯分志願
    原本為檔僑先部和香港ＤＳＥ

--- src/js/result.js
+++ src/js/result.js
@@ -104,7 +104,7 @@
 					}
 					// XXX: id 可能會變動
 					// 如果 apply_way id 是 1, 11 ,79 （以香港中學文憑考試成績 (DSE)、以香港高級程度會考成績 (ALE)、以香港中學會考成績 (CEE)申請、以僑先部結業成績申請），就不顯示分發志願。
-					if (_hasPlacement && (progressJson.student_misc_data.admission_placement_apply_way != 1 && progressJson.student_misc_data.admission_placement_apply_way != 11 && progressJson.student_misc_data.admission_placement_apply_way != 79)) {
+					if (_hasPlacement && (progressJson.student_misc_data.admission_placement_apply_way != 1 )) {
 						const url = '/students/admission-placement-order';
 						const placementResponse = await student.getOrderResultList(url);
 						if (!placementResponse.ok) { throw placementResponse; }
