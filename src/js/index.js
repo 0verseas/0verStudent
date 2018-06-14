0ea19b910e05726f39f2645fc05dd2df84b526b4
He-Wen Chen <lotus3698@gmail.com>
2018-06-14 11:28:12 +0800

    後填志願的，登入後導到填志願頁面

--- src/js/index.js
+++ src/js/index.js
@@ -61,8 +61,16 @@
 			if( typeof(json.student_qualification_verify) === 'undefined')
 				location.href = './systemChoose.html';
 
-			if(json.student_qualification_verify.identity=== 6 && json.student_misc_data.join_admission_selection=== 1
-				&& json.student_misc_data.confirmed_at !=null && json.can_admission_placement == true){
+			if( (json.student_qualification_verify.identity=== 6 &&
+					json.student_misc_data.join_admission_selection=== 1 &&
+					json.student_misc_data.confirmed_at !=null &&
+					json.can_admission_placement == true) ||
+				(data.student_qualification_verify.identity === 7 &&
+					data.student_misc_data.confirmed_at != null &&
+					data.student_misc_data.confirmed_placement_at === null) ||
+				(data.student_misc_data.admission_placement_apply_way_data.code == "23" &&
+					data.student_misc_data.confirmed_at != null &&
+					data.student_misc_data.confirmed_placement_at === null) ){
 				location.href = './placementSelection.html';
 			}
 			else if (!!json.student_misc_data.confirmed_at) {
