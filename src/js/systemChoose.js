9ea25b904830570316f3c5526ac1638b1891cb29
richegg <zxp86021@gmail.com>
2018-06-25 11:18:51 +0800

    systemChoose 未登入自動導向登入頁面

--- src/js/systemChoose.js
+++ src/js/systemChoose.js
@@ -21,21 +21,17 @@
 	*/
 
 	async function _init() {
-		student.isLogin().catch((err) => {
-            if (err.status && err.status === 401) {
-                alert('請登入。');
-                location.href = "./index.html";
-            } else {
-                err.json && err.json().then((data) => {
-                    console.error(data);
-                    alert(`ERROR: \n${data.messages[0]}`);
-                })
-            }
-        });
-
 		try {
 			const response = await student.getStudentRegistrationProgress();
-			if (!response.ok) { throw response; }
+			console.log(response.status);
+			if (!response.ok) {
+				if (response.status && response.status === 401) {
+                    alert('請登入。');
+                    location.href = "./index.html";
+				} else {
+                    throw response;
+                }
+			}
 			const json = await response.json();
 
 			if (json.student_qualification_verify) {
@@ -45,6 +41,7 @@
 		} catch (error) {
 			console.log(error);
 		}
+
 		loading.complete();
 	}
 
