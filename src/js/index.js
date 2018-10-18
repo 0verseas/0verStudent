3fc0455fdd97050fcc54ae14315dcf19f88fd0b9
richegg <zxp86021@gmail.com>
2018-10-18 16:01:18 +0800

    下載連結改為動態產生

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@
 	const $email = $('#email');
 	const $pass = $('#password');
 	const $loginBtn = $('#btn-login');
+	const $downloadLinks = $('#download-links');
 
 	/**
 	*	init
@@ -26,15 +27,25 @@
 
 	async function _init() {
 		try {
-		const response = await student.getAdmissionCount();
-		if (!response.ok) { throw response; }
-		const admissionCount = await response.json();
-		$HK_MacaoNum.text(admissionCount.HK_Macao);
-		$otherNum.text(admissionCount.other);
-
+            $downloadLinks.append(
+            	'<a href="' + env.baseUrl + '/forms/2018志願選填系統操作說明書(香港DSE、CEE、ALE學生適用).pdf" target="_blank" class="list-group-item list-group-item-action">2018志願選填系統操作說明書(香港DSE、CEE、ALE學生適用)</a>' +
+				'<a href="' + env.baseUrl + '/forms/香港各會考文憑成績核計方式及分數換算對照表.pdf" target="_blank" class="list-group-item list-group-item-action">香港各會考文憑成績核計方式及分數換算對照表</a>\n' +
+                '<a href="' + env.baseUrl + '/forms/放棄聯合分發聲明書.pdf" target="_blank" class="list-group-item list-group-item-action">放棄聯合分發聲明書（香港專用）</a>\n' +
+                '<a href="http://get.adobe.com/tw/reader/" target="_blank" class="list-group-item list-group-item-action">觀看中文PDF軟體</a>\n' +
+                '<a href="http://www.7-zip.org/" target="_blank" class="list-group-item list-group-item-action">解壓縮軟體</a>\n' +
+                '<a href="' + env.baseUrl + '/admission-data-correction-form" target="_blank" class="list-group-item list-group-item-action">報名資料修正表</a>\n' +
+                '<a href="http://www.gov.hk/tc/about/helpdesk/softwarerequirement/hkscs.htm" target="_blank" class="list-group-item list-group-item-action">香港字庫(For win2000/XP OR Linux)</a>\n' +
+                '<a href="http://www.ccli.gov.hk/tc_chi/faq/faq_win7-hkscs.html" target="_blank" class="list-group-item list-group-item-action">香港字庫(FOR Win7)</a>'
+			);
+			const response = await student.getAdmissionCount();
+			if (!response.ok) { throw response; }
+			const admissionCount = await response.json();
+			$HK_MacaoNum.text(admissionCount.HK_Macao);
+			$otherNum.text(admissionCount.other);
 		} catch (e) {
 			console.log(e);
 		}
+
 		loading.complete();
 	}
 
@@ -45,7 +56,7 @@
 		const loginData = {
 			email: email,
 			password: sha256(pass)
-		}
+		};
 
 		loading.start();
 		student.login(loginData)
