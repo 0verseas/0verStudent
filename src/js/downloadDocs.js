2443a35be030cd540ab4fe4b54c5ce2dc32be5fa
He-Wen Chen <lotus3698@gmail.com>
2018-10-27 17:23:36 +0800

    修改提醒文字

--- src/js/downloadDocs.js
+++ src/js/downloadDocs.js
@@ -47,9 +47,13 @@
 			}
 			if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
 				data.student_qualification_verify.identity > 3) {
-				$memo.html("請在期限內列印並繳交或郵寄至海外聯合招生委員會。");
+				$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />" +
+					"2、 報名「個人申請」者，務必於須於西元2019年1月2日（星期三）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+					"按下『確認上傳資料並提交』。");
 			} else {
-				$memo.html("請在期限內列印並繳交至駐外機構。");
+				$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
+					"2、 報名「個人申請」者，務必於須於西元2019年1月2日（星期三）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+					"按下『確認上傳資料並提交』。");
 			}
 		})
 		.then(() => {
