44dc8a30d29470c03aadea06d9a19b3996f91548
hms5232 <hms5232@yahoo.com.tw>
2019-01-04 17:01:42 +0800

    增加給師長的推薦函上傳頁面（開發中

---
+++ src/js/recommendLetterUpload.js
@@ -0,0 +1,56 @@
+(() => {
+    /**
+     * init
+     */
+    _verify();
+
+    /**
+     * private method
+     */
+    async function _verify(){
+        loading.complete();
+        /*try {
+            const email = _getParam('email', window.location.href);
+            const token = _getParam('token', window.location.href);
+
+            const response = await student.verifyEmail(email, token);
+            if (!response.ok) { throw response; }
+
+            $('#alert-valid').show();
+            setTimeout(() => {
+                location.href = './index.html';
+            }, 3000);
+            loading.complete();
+        } catch (e) {
+            e.json && e.json().then((data) => {
+                console.error(data);
+
+                if (e.status && e.status === 400) {
+                    $('#alert-invalid').show();
+
+                    setTimeout(() => {
+                        location.href = './index.html';
+                    }, 3000);
+                } else {
+                    alert(`${data.messages[0]}`);
+
+                    setTimeout(() => {
+                        location.href = './index.html';
+                    }, 0);
+                }
+
+                loading.complete();
+            });
+        }*/
+    }
+
+    function _getParam(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+        const results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+})();
