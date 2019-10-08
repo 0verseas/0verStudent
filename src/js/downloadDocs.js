e2efc074ac0c0ba76a1cfb47b5b56c53484ae918
ricklai82 <ricklai82@gmail.com>
2019-10-08 11:59:08 +0800

    Update Date String on downloadDocs

--- src/js/downloadDocs.js
+++ src/js/downloadDocs.js
@@ -52,7 +52,7 @@
 						if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
 							data.student_qualification_verify.identity > 3) {
 							$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />" +
-								"2、 報名「個人申請」者，務必於須於西元2019年1月2日（星期三）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+								"2、 報名「個人申請」者，務必於須於西元2019年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
 								"按下『確認上傳資料並提交』。");
 						} else if (data.student_qualification_verify.system_id === 2) {
 							$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
@@ -64,7 +64,7 @@
 						}
 						else {
 							$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
-								"2、 報名「個人申請」者，務必於須於西元2019年1月2日（星期三）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+								"2、 報名「個人申請」者，務必於須於西元2019年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
 								"按下『確認上傳資料並提交』。");
 						}
 					})
