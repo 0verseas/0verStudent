9d2e74b44fce6872592e2f5610111100f1bbe7ce
ricklai82 <ricklai82@gmail.com>
2019-10-29 16:10:42 +0800

    修改：將直接跳轉到指定改成動畫呈現。

--- src/js/systemChoose.js
+++ src/js/systemChoose.js
@@ -10,7 +10,7 @@
 	*	bind event
 	*/
 	_init();
-
+	
 	/**
 	*	bind event
 	*/
@@ -19,6 +19,25 @@
 	/**
 	*	event handler
 	*/
+
+	const ScrollTop = (number = 0, time) => {
+		if (!time) {
+			document.body.scrollTop = document.documentElement.scrollTop = number;
+			return number;
+		}
+		const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
+		let spacingInex = time / spacingTime; // 计算循环的次数
+		let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
+		let everTop = (number - nowTop) / spacingInex; // 计算每次滑动的距离
+		let scrollTimer = setInterval(() => {
+			if (spacingInex > 0) {
+				spacingInex--;
+				ScrollTop(nowTop += everTop);
+			} else {
+				clearInterval(scrollTimer); // 清除计时器
+			}
+		}, spacingTime);
+	};
 
 	async function _init() {
 		try {
@@ -32,7 +51,8 @@
 			if (json.student_qualification_verify) {
 				$system.val(json.student_qualification_verify.system_id);
 			}
-			window.location.hash = "#selectSystem";
+			console.log(document.body.scrollHeight);
+			ScrollTop(document.body.scrollHeight/2.2,800);
 		} catch (error) {
 			console.log(error);
 		}
