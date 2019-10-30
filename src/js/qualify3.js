49020b854d45058e0ac2380ff07799a3cfaefba3
ricklai82 <ricklai82@gmail.com>
2019-10-30 13:45:14 +0800

    函式加註  更改細節

--- src/js/qualify3.js
+++ src/js/qualify3.js
@@ -10,21 +10,21 @@
 	let _savedSystem = null;
 	let _countryList = [];
 
-	const ScrollTop = (number = 0, time) => {
+	const smoothScroll = (number = 0, time) => {
 		if (!time) {
 			document.body.scrollTop = document.documentElement.scrollTop = number;
 			return number;
 		}
-		const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
-		let spacingInex = time / spacingTime; // 计算循环的次数
-		let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
-		let everTop = (number - nowTop) / spacingInex; // 计算每次滑动的距离
+		const spacingTime = 20; // 動畫循環間隔
+		let spacingInex = time / spacingTime; // 計算動畫次數
+		let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 擷取當前scrollbar位置
+		let everTop = (number - nowTop) / spacingInex; // 計算每次動畫的滑動距離
 		let scrollTimer = setInterval(() => {
 			if (spacingInex > 0) {
-				spacingInex--;
-				ScrollTop(nowTop += everTop);
+				spacingInex--;	
+				smoothScroll(nowTop += everTop); //在動畫次數結束前要繼續滑動
 			} else {
-				clearInterval(scrollTimer); // 清除计时器
+				clearInterval(scrollTimer); // 結束計時器
 			}
 		}, spacingTime);
 	};
@@ -92,8 +92,8 @@
 			}
 			loading.complete();
 		});
-		if(document.body.scrollWidth<768)
-		ScrollTop(document.body.scrollHeight/2.0,800);
+		if(document.body.scrollWidth<768)  // 判別網頁寬度 少於768會今入單欄模式
+		smoothScroll(document.body.scrollHeight/2.2,800);  // 用整體長度去做計算  滑動到需要填寫欄位位置
 	}
 
 	/**
--- src/js/systemChoose.js
+++ src/js/systemChoose.js
@@ -6,6 +6,25 @@
 	const $nextBtn = $SystemChoose.find('.SystemChoose__btnNext');
 	const $system = $SystemChoose.find('#SystemChoose__inputSystem');
 
+	const smoothScroll = (number = 0, time) => {
+		if (!time) {
+			document.body.scrollTop = document.documentElement.scrollTop = number;
+			return number;
+		}
+		const spacingTime = 20; // 動畫循環間隔
+		let spacingInex = time / spacingTime; // 計算動畫次數
+		let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 擷取當前scrollbar位置
+		let everTop = (number - nowTop) / spacingInex; // 計算每次動畫的滑動距離
+		let scrollTimer = setInterval(() => {
+			if (spacingInex > 0) {
+				spacingInex--;	
+				smoothScroll(nowTop += everTop); //在動畫次數結束前要繼續滑動
+			} else {
+				clearInterval(scrollTimer); // 結束計時器
+			}
+		}, spacingTime);
+	};
+
 	/**
 	*	bind event
 	*/
@@ -19,26 +38,7 @@
 	/**
 	*	event handler
 	*/
-
-	const ScrollTop = (number = 0, time) => {
-		if (!time) {
-			document.body.scrollTop = document.documentElement.scrollTop = number;
-			return number;
-		}
-		const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
-		let spacingInex = time / spacingTime; // 计算循环的次数
-		let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
-		let everTop = (number - nowTop) / spacingInex; // 计算每次滑动的距离
-		let scrollTimer = setInterval(() => {
-			if (spacingInex > 0) {
-				spacingInex--;
-				ScrollTop(nowTop += everTop);
-			} else {
-				clearInterval(scrollTimer); // 清除计时器
-			}
-		}, spacingTime);
-	};
-
+	
 	async function _init() {
 		try {
 			const response = await student.getStudentRegistrationProgress();
@@ -51,8 +51,8 @@
 			if (json.student_qualification_verify) {
 				$system.val(json.student_qualification_verify.system_id);
 			}
-			if(document.body.scrollWidth<768)
-			ScrollTop(document.body.scrollHeight/2.2,800);
+			if(document.body.scrollWidth<768)  // 判別網頁寬度 少於768會今入單欄模式
+			smoothScroll(document.body.scrollHeight/2.2,800);  // 用整體長度去做計算  滑動到需要填寫欄位位置	
 		} catch (error) {
 			console.log(error);
 		}
