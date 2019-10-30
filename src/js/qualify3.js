4939c383c0b0daf74e4abe3d725b9052b21ca883
ricklai82 <ricklai82@gmail.com>
2019-10-30 11:25:52 +0800

    將計算座標移動改成平滑移動至錨點方便適應不同裝置

--- src/js/qualify3.js
+++ src/js/qualify3.js
@@ -10,23 +10,9 @@
 	let _savedSystem = null;
 	let _countryList = [];
 
-	const ScrollTop = (number = 0, time) => {
-		if (!time) {
-			document.body.scrollTop = document.documentElement.scrollTop = number;
-			return number;
-		}
-		const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
-		let spacingInex = time / spacingTime; // 计算循环的次数
-		let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
-		let everTop = (number - nowTop) / spacingInex; // 计算每次滑动的距离
-		let scrollTimer = setInterval(() => {
-			if (spacingInex > 0) {
-				spacingInex--;
-				ScrollTop(nowTop += everTop);
-			} else {
-				clearInterval(scrollTimer); // 清除计时器
-			}
-		}, spacingTime);
+	const ScrollTop = () => {
+		let element = document.getElementById("identityCheck");
+		element.scrollIntoView({behavior:  "smooth"});
 	};
 
 	/**
@@ -93,7 +79,7 @@
 			loading.complete();
 		});
 		if(document.body.scrollWidth<768)
-		ScrollTop(document.body.scrollHeight/2.0,800);
+		ScrollTop();
 	}
 
 	/**
