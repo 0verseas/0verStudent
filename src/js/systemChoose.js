2666ffc96f769d2ea111328030df8d2a6deb06be
hms5232 <hms5232@yahoo.com.tw>
2019-10-30 13:09:42 +0800

    Revert "將計算座標移動改成平滑移動至錨點方便適應不同裝置"
    This reverts commit 4939c383c0b0daf74e4abe3d725b9052b21ca883.

--- src/js/systemChoose.js
+++ src/js/systemChoose.js
@@ -20,9 +20,23 @@
 	*	event handler
 	*/
 
-	const ScrollTop = () => {
-		let element = document.getElementById("SystemChoose__inputSystem");
-		element.scrollIntoView({behavior:  "smooth"});
+	const ScrollTop = (number = 0, time) => {
+		if (!time) {
+			document.body.scrollTop = document.documentElement.scrollTop = number;
+			return number;
+		}
+		const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
+		let spacingInex = time / spacingTime; // 计算循环的次数
+		let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
+		let everTop = (number - nowTop) / spacingInex; // 计算每次滑动的距离
+		let scrollTimer = setInterval(() => {
+			if (spacingInex > 0) {
+				spacingInex--;
+				ScrollTop(nowTop += everTop);
+			} else {
+				clearInterval(scrollTimer); // 清除计时器
+			}
+		}, spacingTime);
 	};
 
 	async function _init() {
@@ -38,7 +52,7 @@
 				$system.val(json.student_qualification_verify.system_id);
 			}
 			if(document.body.scrollWidth<768)
-			ScrollTop();
+			ScrollTop(document.body.scrollHeight/2.2,800);
 		} catch (error) {
 			console.log(error);
 		}
