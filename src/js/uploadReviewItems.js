491319fa5523ca58ba13def95918a2cbac6fd2f4
hms5232 <hms5232@yahoo.com.tw>
2019-12-09 15:47:02 +0800

    在師長推薦函邀請列表上方新增提醒框框：鎖定後就不能動了喔請小心>_^

--- src/js/uploadReviewItems.js
+++ src/js/uploadReviewItems.js
@@ -806,6 +806,7 @@
 	function getTeacherRecommendationLetterInviteRecord(fileListItem) {
 		let invite_table = `
 			<div id="invitation-area" class="col-md-11" style=" margin:10px auto 28px;overflow-x:auto;">
+				<div class="alert alert-danger"><b>小心！ </b><strong>提交後師長即無法再上傳或更動推薦函</strong>，請確認上傳情況後再提交備審資料！</div>
 				<table id="invitation-list">
 					<tr>
 						<th>師長姓名</th>
