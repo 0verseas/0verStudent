7bbbcf641070b2cfff2061efc66f7884d733f6d5
ricklai82 <ricklai82@gmail.com>
2020-02-06 13:45:35 +0800

    將規則整理後並重構

--- src/js/downloadDocs.js
+++ src/js/downloadDocs.js
@@ -43,43 +43,71 @@
 								$('#alert-hk-order').show();
 							}
 						}
-						
+
 						if ((data.student_qualification_verify.system_id === 1 || data.student_qualification_verify.system_id === 2)
 							&& (data.student_misc_data.admission_placement_apply_way === 6 || data.student_misc_data.admission_placement_apply_way === 16)) { // 參加澳門學科測驗
 							$('#alert-cost').show();
 							$('.alert-downloadMoFile').show();
 						}
-						//TODO : 這部份感覺有機會可以整理條件之後重構成比較簡單的寫法
-						if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
-							data.student_qualification_verify.identity > 3) {
-							if(data.can_admission_selection){
-								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />"+
-								"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
-								"按下『確認上傳資料並提交』。");
-							} else {
-								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
-							}
-						} else if (data.student_qualification_verify.system_id === 2) {
-							if(data.can_admission_selection){
-								$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
-								"2、 報名「港二技」者，務必於西元2020年3月21日（星期六）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
-								"按下『確認上傳資料並提交』。");
-							} else {
-								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
-							}
-						}
-						else if (data.student_qualification_verify.identity === 6) {
-                            $memo.html("請在簡章規定之期限內列印並繳交至國立台灣師範大學僑先部教務組。");
-						}
-						else {
-							if(data.can_admission_selection){
-								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />"+
-								"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
-								"按下『確認上傳資料並提交』。");
-							} else {
-								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
+						//嘗試重構後  先判斷 是不是僑先部結業生
+						if(data.student_qualification_verify.identity === 6) {
+							$memo.html("請在簡章規定之期限內列印並繳交至國立台灣師範大學僑先部教務組。");
+						} else { //不是就先判斷是否在個人申請時間內
+							if(!data.can_admission_selection){ //非個人申請時間內有兩種情況 1. 在台碩博  2. 其他
+								if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
+								data.student_qualification_verify.identity > 3) {
+									$memo.html("請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
+								}else{
+									$memo.html("請在簡章規定之期限內列印並繳交至駐外機構。<br />");
+								}
+							} else { //在個人申請時間內有三種情況  1.在台碩博  2.港二技  3.其他
+								if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
+								data.student_qualification_verify.identity > 3) {
+									$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />"+
+									"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+									"按下『確認上傳資料並提交』。");
+								} else if (data.student_qualification_verify.system_id === 2) {
+									$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
+									"2、 報名「港二技」者，務必於西元2020年3月21日（星期六）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+									"按下『確認上傳資料並提交』。");
+								} else {
+									$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />"+
+									"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+									"按下『確認上傳資料並提交』。");
+								}
 							}
 						}
+						//TODO :  先保留舊有規則   之後double確認後就可以刪除了
+						// if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
+						// 	data.student_qualification_verify.identity > 3) {
+						// 	if(data.can_admission_selection){
+						// 		$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />"+
+						// 		"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+						// 		"按下『確認上傳資料並提交』。");
+						// 	} else {
+						// 		$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
+						// 	}
+						// } else if (data.student_qualification_verify.system_id === 2) {
+						// 	if(data.can_admission_selection){
+						// 		$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
+						// 		"2、 報名「港二技」者，務必於西元2020年3月21日（星期六）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+						// 		"按下『確認上傳資料並提交』。");
+						// 	} else {
+						// 		$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
+						// 	}
+						// }
+						// else if (data.student_qualification_verify.identity === 6) {
+                        //     $memo.html("請在簡章規定之期限內列印並繳交至國立台灣師範大學僑先部教務組。");
+						// }
+						// else {
+						// 	if(data.can_admission_selection){
+						// 		$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />"+
+						// 		"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+						// 		"按下『確認上傳資料並提交』。");
+						// 	} else {
+						// 		$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />");
+						// 	}
+						// }
 					})
 					.then(() => {
 						$('#btn-smart').attr('href', env.baseUrl + '/students/print-admission-paper');
