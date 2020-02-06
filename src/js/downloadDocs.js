921cad426ba44c1aa944fd3888a8dc5457755a4c
ricklai82 <ricklai82@gmail.com>
2020-02-06 11:43:41 +0800

    會確認是否有在開放個人申請時間內來決定顯示文字

--- src/js/downloadDocs.js
+++ src/js/downloadDocs.js
@@ -43,29 +43,42 @@
 								$('#alert-hk-order').show();
 							}
 						}
-
+						
 						if ((data.student_qualification_verify.system_id === 1 || data.student_qualification_verify.system_id === 2)
 							&& (data.student_misc_data.admission_placement_apply_way === 6 || data.student_misc_data.admission_placement_apply_way === 16)) { // 參加澳門學科測驗
 							$('#alert-cost').show();
 							$('.alert-downloadMoFile').show();
 						}
+						//TODO : 這部份感覺有機會可以整理條件之後重構成比較簡單的寫法
 						if ( (data.student_qualification_verify.system_id === 3 || data.student_qualification_verify.system_id === 4) &&
 							data.student_qualification_verify.identity > 3) {
-							$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />" +
+							if(data.can_admission_selection){
+								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />"+
 								"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
 								"按下『確認上傳資料並提交』。");
+							} else {
+								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
+							}
 						} else if (data.student_qualification_verify.system_id === 2) {
-							$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
+							if(data.can_admission_selection){
+								$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
 								"2、 報名「港二技」者，務必於西元2020年3月21日（星期六）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
 								"按下『確認上傳資料並提交』。");
+							} else {
+								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
+							}
 						}
 						else if (data.student_qualification_verify.identity === 6) {
                             $memo.html("請在簡章規定之期限內列印並繳交至國立台灣師範大學僑先部教務組。");
 						}
 						else {
-							$memo.html("1、 請在簡章規定之期限內列印並繳交至駐外機構。<br />" +
-								"2、 報名「個人申請」或「研究所」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
+							if(data.can_admission_selection){
+								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />"+
+								"2、 報名「個人申請」者，務必於西元2020年1月6日（星期一）臺灣時間下午5時前完成備審資料上傳作業，<br />" +
 								"按下『確認上傳資料並提交』。");
+							} else {
+								$memo.html("1、 請在簡章規定之期限內列印並繳交或郵寄至海外聯合招生委員會。<br />");
+							}
 						}
 					})
 					.then(() => {
