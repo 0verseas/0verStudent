dd5d85777275d41d40ffac296000da933282237d
ricklai <ricklai82@gmail.com>
2020-05-05 10:43:16 +0800

    四校聯考成績上傳，前端調整。

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -424,8 +424,8 @@
 			$macautranscript.hide();
 			$macauTranscriptAlert.hide();
 		}else if(!json.can_macau_upload_time){ //確認現在時間是否在開放時間內  不是就改變按鈕狀態
-			$macautranscript.show().prop('disabled', true).text('目前不是登錄四校聯考成績時間');
-			$macauTranscriptAlert.show().text('開放時間：2020 年 5 月 1 日 00：00：00');
+			$macautranscript.show().prop('disabled', true).text('非四校聯考成績登錄開放時間');
+			$macauTranscriptAlert.show().text('');
 			//$macauTranscriptAlert.hide();
 		}else if( json.student_misc_data.overseas_student_id == null){ //確認是否有僑生編號 沒有就請學生等待審核
 			$macautranscript.show().prop('disabled', true).text('目前不能登錄上傳四校聯考成績');
