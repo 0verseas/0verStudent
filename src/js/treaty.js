13fceb29b28fa1e52a1965e14bfd2c3875788fa0
ricklai <ricklai82@gmail.com>
2020-05-05 13:41:14 +0800

    個資條例前面加上checkbox。

--- src/js/treaty.js
+++ src/js/treaty.js
@@ -3,6 +3,15 @@
 	/**
 	*	cache DOM
 	*/
+	$chechBox2 = $('#agreeLawCheck2');
+	$chechBox3 = $('#agreeLawCheck3');
+	$chechBox4 = $('#agreeLawCheck4');
+	$chechBox5 = $('#agreeLawCheck5');
+	$chechBox6 = $('#agreeLawCheck6');
+	$chechBox7 = $('#agreeLawCheck7');
+	$chechBox8 = $('#agreeLawCheck8');
+	$chechBox9 = $('#agreeLawCheck9');
+	$chechBox10 = $('#agreeLawCheck10');
 	
 	/**
 	*	init
@@ -13,9 +22,33 @@
 	/**
 	*	bind event
 	*/
+	$chechBox2.on('change',_handleCheck);
+	$chechBox3.on('change',_handleCheck);
+	$chechBox4.on('change',_handleCheck);
+	$chechBox5.on('change',_handleCheck);
+	$chechBox6.on('change',_handleCheck);
+	$chechBox7.on('change',_handleCheck);
+	$chechBox8.on('change',_handleCheck);
+	$chechBox9.on('change',_handleCheck);
+	$chechBox10.on('change',_handleCheck);
 
 	function _init() {
 		loading.complete();
+	}
+
+	function _handleCheck(){
+		let checkAll = 1;
+		for(let i=0; i<9;i++){
+			if(!$('#agreeLawCheck'+(i+2))[0].checked){
+				checkAll = 0;
+				break;
+			}
+		}
+		if(checkAll === 1){
+			$('#btn-agree').prop('disabled', false);
+		} else {
+			$('#btn-agree').prop('disabled', true);
+		}
 	}
 
 
