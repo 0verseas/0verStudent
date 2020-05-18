c88462409b53fe0afd499c6a9f3febcfca0dd5d2
ricklai <ricklai82@gmail.com>
2020-05-18 17:24:40 +0800

    tab版本 checkbox 精簡

--- src/js/treaty.js
+++ src/js/treaty.js
@@ -3,15 +3,10 @@
 	/**
 	*	cache DOM
 	*/
-	const $checkBox2 = $('#agreeLawCheck2');
-	const $checkBox3 = $('#agreeLawCheck3');
-	const $checkBox4 = $('#agreeLawCheck4');
-	const $checkBox5 = $('#agreeLawCheck5');
-	const $checkBox6 = $('#agreeLawCheck6');
-	const $checkBox7 = $('#agreeLawCheck7');
-	const $checkBox8 = $('#agreeLawCheck8');
-	const $checkBox9 = $('#agreeLawCheck9');
-	const $checkBox10 = $('#agreeLawCheck10');
+	const $chinesesAgreeCheckBox = $('#agree-check-chinese');
+	const $englishAgreeCheckBox = $('#agree-check-english');
+	const $agreeButton = $('#btn-agree');
+	let agreeCheck = false;
 	
 	/**
 	*	init
@@ -22,32 +17,33 @@
 	/**
 	*	bind event
 	*/
-	$checkBox2.on('change',_handleCheck);
-	$checkBox3.on('change',_handleCheck);
-	$checkBox4.on('change',_handleCheck);
-	$checkBox5.on('change',_handleCheck);
-	$checkBox6.on('change',_handleCheck);
-	$checkBox7.on('change',_handleCheck);
-	$checkBox8.on('change',_handleCheck);
-	$checkBox9.on('change',_handleCheck);
-	$checkBox10.on('change',_handleCheck);
+	$chinesesAgreeCheckBox.on('change',_handleCheck);
+	$englishAgreeCheckBox.on('change',_handleCheck);
+	$agreeButton.on('click',_handleAgree);
 
 	function _init() {
 		loading.complete();
 	}
 
 	function _handleCheck(){
-		let checkAll = 1;
-		for(let i=0; i<9;i++){
-			if(!$('#agreeLawCheck'+(i+2))[0].checked){
-				checkAll = 0;
-				break;
-			}
-		}
-		if(checkAll === 1){
-			$('#btn-agree').prop('disabled', false);
+		console.log($chinesesAgreeCheckBox);
+		console.log($englishAgreeCheckBox);
+		if($(this)[0].checked){
+			$chinesesAgreeCheckBox[0].checked = true;
+			$englishAgreeCheckBox[0].checked = true;
+			agreeCheck = true;
 		} else {
-			$('#btn-agree').prop('disabled', true);
+			$chinesesAgreeCheckBox[0].checked = false;
+			$englishAgreeCheckBox[0].checked = false;
+			agreeCheck = false;
+		}
+	}
+
+	function _handleAgree(){
+		if(agreeCheck){
+			location.href='./register.html'
+		} else {
+			alert('你必須詳讀所有條款內容，並在最後勾選表示同意。\n You have to read the all the terms thoroughly, and then check the checkbox in the end.')
 		}
 	}
 
