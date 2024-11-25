98df320f95fd2bb4bff62db6c48527dc878facfc
ricklai82 <ricklai82@gmail.com>
2024-11-25 16:15:12 +0800

    因行動裝置相容性問題，將有用到 embed tag 的頁面替換成 iframe tag。

--- src/js/uploadEducation.js
+++ src/js/uploadEducation.js
@@ -254,7 +254,7 @@
 		} else {
 			$imgModalBody.html(`
 				<div style="margin: 0 auto">
-					<embed src="${this.dataset.filelink}" width="550" height="800" type="application/pdf">
+					<iframe src="${this.dataset.filelink}" width="550" height="800" type="application/pdf">
 				</div>
 			`);
 		}
